227274d995f9263d2901fdfb43892d3e8fbaf770
bricesh <bricesh@yahoo.com>
2015-10-21 07:23:18 +0100

    Version for sunmission

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Implementation of cached matrix inverse
 
-## Write a short comment describing this function
+## Function to define matrix object with extra methods to enable the use of cached values
 
 makeCacheMatrix <- function(x = matrix()) {
-    ## not shure what to do
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(minv) m <<- minv
+    getinv <- function() m
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## function to warp around solve() which uses methods defined in makeCacheMatrix()
+## that cached the value of inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-    ## still not sure what to do
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
 }
+
+## USAGE:
+##
+## source("cachematrix.R")
+## X <- makeCacheMatrix(matrix(c(1, 3, 5, 4, 3, 7, 8, 10, 5), nrow = 3, ncol = 3))
+## cacheSolve(X)
